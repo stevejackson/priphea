24b3004419880a7eac9a7495137c69aa0b93270c
Steve Jackson <steven.j.jackson@gmail.com>
2015-05-13 20:06:25 -0400

    Create shell file to conveniently start the app.
    Former-commit-id: b147b9ceaf114933e7128ebdac1b057c2203ebe0

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "Starting up Priphea in production mode..."
+
+echo "-----"
+echo "Installing bundle..."
+cd /Users/steve/dev/priphea_production
+bundle install
+
+echo "-----"
+echo "Compiling assets..."
+RAILS_ENV=production rake assets:precompile
+
+echo "-----"
+echo "Attempting to kill existing rails server..."
+kill -9 $(cat tmp/pids/server.pid)
+kill -9 $(cat tmp/pids/thin.3456.pid)
+
+echo "-----"
+echo "Starting up new rails server..."
+RAILS_ENV=production rails s thin -p 3456 &> /dev/null &
+
+echo "-----"
+echo "Starting up guard to monitor file notifications..."
+bundle exec guard &> /dev/null &
+
+echo "-----"
+echo "Starting up cmus for audio playback..."
+cmus
