040363c8fcdb44480242e31b9fe639d7874542d0
Steve Jackson <steven.j.jackson@gmail.com>
2015-06-04 00:09:40 -0400

    Disable database output in production.
    Former-commit-id: 8938942f53973012f74c218f6389132707d6e9a1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Mongoid.logger.level = Logger::DEBUG
-  # Mongoid.raise_not_found_error = false
+  Mongoid.logger = false # disable mongoid logger
+  Moped.logger = Logger.new(StringIO.new) # disable moped logger
+  Mongoid.raise_not_found_error = false
 
   # Code is not reloaded between requests.
   config.cache_classes = true
