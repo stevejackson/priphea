8100f9ad267d40f564f7b8c262eada3e5ce32965
Steven Jackson <steve@sjackson.net>
2016-02-04 22:50:19 +0800

    Add a pending test for album changing
    Former-commit-id: 3822f9595280573b00c766a3350068ea1502ef6d

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -5,4 +5,34 @@ describe Song do
   # validations
   it { should validate_presence_of(:album) }
 
+  context "#write_metadata_to_file" do
+    it "can reassign song to a different album if the album changed" do
+      pending "Not implemented"
+      fail
+      @files.each do |file|
+        @scanner = Scanner.new(Settings.library_path)
+        @song = Song.find_by(full_path: file)
+
+        previous_album_id = @song.album_id
+        next_album = Album.where.not(id: previous_album_id).first
+
+        new_album_artist = "Kaboom"
+
+        @song.album_id = 0 # TODO
+
+
+        expect(@song.write_metadata_to_file!).to be_truthy
+
+        metadata = AudioMetadata::from_file(@song.full_path)
+        expect(metadata['title']).to eq(new_title)
+        expect(metadata['comment']).to eq(new_comment)
+        expect(metadata['track_number']).to eq(new_track_number)
+        expect(metadata['disc_number']).to eq(new_disc_number)
+        expect(metadata['artist']).to eq(new_artist)
+        expect(metadata['album_artist']).to eq(new_album_artist)
+      end
+    end
+  end
+
+
 end
