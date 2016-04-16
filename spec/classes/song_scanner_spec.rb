715760dd225ce79efaddac264c12e2ab9b4ec54e
Steven Jackson <steve@sjackson.net>
2016-04-16 23:31:19 +0800

    Get failing test passing
    Former-commit-id: 6195346d0dc9585a4def3ed03763158906989d7a

--- spec/classes/song_scanner_spec.rb
+++ spec/classes/song_scanner_spec.rb
@@ -4,12 +4,12 @@ describe SongScanner do
   context ".file_is_missing" do
     it "should mark song as missing if file no longer exists" do
       path = "abc/123.mp3"
-      song = create(:active_song, full_path: path)
+      create(:active_song, full_path: path)
 
+      song_scanner = SongScanner.new(path, false)
       expect {
-        song_scanner = SongScanner.new(path, false)
-        song_scanner.scan_file
-      }.to change { song.state }.from("active").to("missing")
+        song_scanner.file_is_missing?
+      }.to change { song_scanner.song.state }.from("active").to("missing")
     end
   end
 end
