b754a9591175b4c75624efb0f8055e9150fc6f4f
Steven Jackson <steve@sjackson.net>
2016-05-14 02:32:46 +0800

    Syntax change

--- spec/services/album_art_purger_spec.rb
+++ spec/services/album_art_purger_spec.rb
@@ -8,7 +8,7 @@ describe AlbumArtPurger, file_cleaning: :full do
     end
 
     it "should remove existing art files from an album directory" do
-      album = Album.where(title: 'Rogue Galaxy Premium Arrange').first
+      album = Album.where(title: "Rogue Galaxy Premium Arrange").first
 
       expect {
         AlbumArtPurger.new(album).purge_existing_art_files
