f1231519a6b39345028180b85f74b84080be129d
Steven Jackson <steve@sjackson.net>
2016-05-22 00:44:34 +0800

    Write spec for AlbumBrowserController#showAlbum

--- spec/javascripts/album_browser/album_browser_controller_spec.js
+++ spec/javascripts/album_browser/album_browser_controller_spec.js
@@ -19,15 +19,27 @@ describe("AlbumBrowserController", function() {
     );
   });
 
-  it('should fetch albums on init', function() {
+  beforeEach(function() {
     httpBackend.expectGET('/api/albums').respond([1]);
+  });
 
+  it('should fetch albums on init', function() {
     var $scope = {};
-    var controller = $controller('albumBrowserController', { $scope: $scope });
+    var controller = $controller('AlbumBrowserController', { $scope: $scope });
     httpBackend.flush();
     expect($scope.albums).to.eql([1]);
   });
 
   describe('#showAlbum', function() {
+    it('should fetch specific album', function() {
+      httpBackend.expectGET('/api/albums/abc123').respond({ 'blah': 123 });
+
+      var $scope = {};
+      var controller = $controller('AlbumBrowserController', { $scope: $scope });
+      $scope.showAlbum('abc123');
+
+      httpBackend.flush();
+      expect($scope.selectedAlbum).to.eql({ 'blah': 123 });
+    });
   });
 });
