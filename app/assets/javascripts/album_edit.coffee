95bad8bee65f95a69d4a412a13af0053ee01e856
Steven Jackson <steve@sjackson.net>
2016-06-02 00:49:20 +0800

    Remove album_edit jQuery code

--- app/assets/javascripts/album_edit.coffee
+++
@@ -1,7 +0,0 @@
-#= require update_album_metadata_form
-
-$ ->
-
-  metadataForm = new UpdateAlbumMetadataForm('update_album_metadata')
-
-
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery-ujs
-//= require album_edit
 //= require angular
 //= require angular-lazy-img
 //= require angular-ui-router
